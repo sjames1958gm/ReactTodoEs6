eaf911a69673ae2daf33c4699aedad870d5e0364
Stephen James <sajames1958@gmail.com>
2016-08-31 21:04:37 -0500

    initial rework

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,7 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
+var TodoApp = require('TodoApp');
 
 // Load foundation
 $(document).foundation();
@@ -10,9 +10,6 @@ $(document).foundation();
 require("style!css!sass!applicationStyles");
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Main}>
-    </Route>
-  </Router>,
+  <TodoApp/>,
   document.getElementById('app')
 );
