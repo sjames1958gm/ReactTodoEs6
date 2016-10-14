b83ea9a6cf3c1e563fce4c9c4f943ce5bb33d7a6
Stephen James <sajames1958@gmail.com>
2016-10-14 04:47:36 -0500

    change db

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 try {
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyDX1mwas6cb7a0h2omihYIZw99rK0vzxO0",
-    authDomain: "sjames-todo-app.firebaseapp.com",
-    databaseURL: "https://sjames-todo-app.firebaseio.com",
-    storageBucket: "sjames-todo-app.appspot.com",
-    messagingSenderId: "446537071503"
+    apiKey: "AIzaSyCda8-lYIwslJzBFIh1uEGMQN1pn1YspTE",
+    authDomain: "mytodoapp-3d23d.firebaseapp.com",
+    databaseURL: "https://mytodoapp-3d23d.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "401649224881"
   };
   
   firebase.initializeApp(config); 
